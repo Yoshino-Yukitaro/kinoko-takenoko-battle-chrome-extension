01ae9506535cd6d82e39c5a7f95327983bca8cc3
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-12-14 18:05:15 +0900

    feat: 最低限のchrome拡張開発環境を整えた

--- manifest.ts
+++ manifest.ts
@@ -1,13 +1,19 @@
-import { defineManifest } from '@crxjs/vite-plugin';
+import { defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
-  manifest_version: 3,
-  name: "サクッときのこたけのこ戦争",
-  version: "1.0.0",
-  permissions: ["activeTab"],
-  action: {
-    default_popup: "popup/index.html",
-  },
+	manifest_version: 3,
+	name: "サクッときのこたけのこ戦争",
+	version: "1.0.0",
+	permissions: ["activeTab"],
+	content_scripts: [
+		{
+			matches: ["http://*/*", "https://*/*"],
+			js: ["content/index.tsx"],
+		},
+	],
+	action: {
+		default_popup: "popup/index.html",
+	},
 });
 
 export default manifest;
